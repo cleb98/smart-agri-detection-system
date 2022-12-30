18f5e67b49ece3698078d0d7bdb2edf42bb55e2a
cleb98 <cribellu98@gmail.com>
2022-12-30 16:39:38 +0100

    bridge done

---
+++ inference iot/Roboflow_inference.py
@@ -0,0 +1,61 @@
+import base64
+import requests
+import urllib.request
+import os 
+from snapshot import counter
+
+
+# Set the path to the output folder
+output_folder = 'I:/Il mio Drive/UNImore/IoT/00_Progetto/bridge/output'
+
+# Read the image from the file in the output folder
+image_filename = 'image{}.jpg'.format(counter)
+image_filepath = os.path.join(output_folder, image_filename)
+with open(image_filepath, 'rb') as f:
+    image_data = f.read()
+
+# # URL dell'immagine da importare
+# image_url = "https://best5.it/b5/wp-content/uploads/2019/07/ape-2-800x400.jpg"
+
+# # Scarica l'immagine
+# with urllib.request.urlopen(image_url) as url:
+#     image_data = url.read()
+
+# # Salva l'immagine in un file
+# with open("image.jpg", "wb") as f:
+#     f.write(image_data)
+
+#inizio counter=0
+
+# do in inferenza image0 per ottenere predict0
+# counter = 0  # counter for generating unique filenames
+def prediction():
+    global counter
+    print(counter)
+    filename = os.path.join(output_folder, 'image{}.jpg'.format(counter))
+    print(filename)
+
+
+    # def model():
+    #     #!pip install Roboflow
+    #     global counter
+    from roboflow import Roboflow
+    rf = Roboflow(api_key="YswyoRwpN8l4oas9n0qJ")
+    project = rf.workspace("iotinsectdetectionproject").project("insects-detection-hndll")
+    model = project.version(6).model
+
+    # infer on a local image
+    print(model.predict(filename, confidence=40, overlap=30).json())
+
+    # visualize your prediction
+    # generate unique filename using the counter
+    prediction_filename = os.path.join(output_folder, 'prediction{}.jpg'.format(counter))
+    model.predict(filename, confidence=40, overlap=30).save(prediction_filename)
+    counter += 1  # increment the counter
+    print(counter)
+
+# if __name__ == '__main__':
+#     try:
+#         prediction()
+#     except Exception as e:
+#         print('You cannot make a prediction!', e)
--- inference iot/snapshot.py
+++ inference iot/snapshot.py
@@ -1,28 +1,40 @@
 import cv2
 import os
 
+counter = 0  # counter for generating unique filenames
+
 def take_photo():
     """
     takes a photo and places it in the output folder
     """
+    global counter  # make counter global so it can be incremented
+
     cap = cv2.VideoCapture(0) # set the camera index. '1' is for default camera 
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) # tune
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240) # tune
     ret, frame = cap.read()
     dataset_folder = os.path.join(os.getcwd(),'output')
-    
+
 
     if not os.path.exists(dataset_folder):
         print('funziona')
         os.makedirs(dataset_folder)
-        
+    # every time you take an image it is saved in file image.jpg
+    # the old image will be deleted,otherwise to keep it use the counter implementation  
+
     # cv2.imwrite('output/image.jpg', frame) # jpg/png
-    cv2.imwrite('image.jpg', frame) # jpg/png
+    # cv2.imwrite('image.jpg', frame) # jpg/png
 
+    #counter implementation 
+    # generate unique filename using the counter every image will be saved in output folder
+    filename = 'output/image{}.jpg'.format(counter)
+    cv2.imwrite(filename, frame) # jpg/png
+    counter += 1  # increment of the counter is done inside Roboflow_inference
     cap.release()
+    #uncomment it for take a photo running the script
 
-if __name__ == '__main__':
-    try:
-        take_photo()
-    except Exception as e:
-        print('You cannot take a photo!', e)
+    # if __name__ == '__main__':
+    #     try:
+    #         take_photo()
+    #     except Exception as e:
+    #         print('You cannot take a photo!', e)
