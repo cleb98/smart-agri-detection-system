362dd22f51d13b241d4675edfa5d7d421b75b26c
olmo <325524@studenti.unimore.it>
2023-02-06 23:48:32 +0100

    update requirements

---
+++ frontend_streamlit/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Insect Detection",
+    page_icon="🪳",
+)
+
+st.markdown("<h1 style='text-align: center;'>🪲 Insect Detection 🪲</h1>", unsafe_allow_html=True)
