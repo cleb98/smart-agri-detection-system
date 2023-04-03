af768b1c996ae4630d08f4e29f787422a5251122
cleb98 <cribellu98@gmail.com>
2023-04-03 11:39:53 +0200

    label for points

--- frontend_streamlit/nearest_neighbor.py
+++ frontend_streamlit/nearest_neighbor.py
@@ -17,14 +17,17 @@ def haversine(point1, point2):
     return c * 6371 * 1000
 
 
+# Define binary classes for each point
+binary_classes = np.array([0, 1])
+
 # Creare una matrice numpy che contiene le coordinate dei punti in formato [latitudine, longitudine] (casa modena, Gubbio)
 points = np.array([[44.63233671612906, 10.93369970312806],[43.35566843537755, 12.42751296772821]])
 
 # Creare un modello NearestNeighbors con la funzione haversine come metrica
 neigh = NearestNeighbors(n_neighbors=2, metric=haversine)
 
-# Fit il modello con la matrice di punti
-neigh.fit(points)
+# Fit il modello con la matrice di punti e le rispettive classi binarie
+neigh.fit(points, binary_classes)
 
 # Chiedere all'utente di inserire le coordinate del punto
 new_point = np.array([float(input("Enter latitude: ")), float(input("Enter longitude: "))]).reshape(1, -1)
@@ -39,8 +42,10 @@ sorted_indices = np.argsort(distances)
 distances = distances[0][sorted_indices[0]]
 indices = indices[0][sorted_indices[0]]
 
-# Print distances and indices in order
-print("Distances to nearest neighbors:")
-for d, i in zip(distances, indices):
-    print(f"Distance: {d}, Index: {i}")
+# Get binary classes for nearest neighbors
+binary_classes = neigh.classes_[indices]
 
+# Print distances, indices, and binary classes in order
+print("Distances to nearest neighbors:")
+for d, i, c in zip(distances, indices, binary_classes):
+    print(f"Distance: {d}, Index: {i}, Class: {c}")
